53e705601059f32e261a905b6f17ce5883711e53
ConorLazarou <conor.lazarou@7shifts.com>
2019-12-14 10:10:46 -0600

    minor

--- visualizing_mnist_12019_12/scripts/main.py
+++ visualizing_mnist_12019_12/scripts/main.py
@@ -8,34 +8,38 @@ import seaborn as sns
 import tensorflow.keras.layers as layers
 from tensorflow.keras.models import Model
 from tensorflow.keras.datasets import mnist
+from tensorflow.keras.initializers import RandomNormal
 from PIL import Image
 from matplotlib.cm import get_cmap
 
 sns.set()
 
 ## Constants and parameters
+'''
 ACTIVATION = 'relu'
 CMAP = get_cmap('Greys_r')
-EPOCHS = 4
+EPOCHS = 5
 BATCH_SIZE = 1024
 PLOT_EVERY = 1
 '''
 ACTIVATION = 'tanh'
 CMAP = get_cmap('seismic_r')
-EPOCHS = 8
+EPOCHS = 5
 BATCH_SIZE = 1024
-PLOT_EVERY = 2
-'''
+PLOT_EVERY = 1
+#'''
 DATA_DIR = f'../{ACTIVATION}/data_log/'
 MODELS_DIR = f'../{ACTIVATION}/models/'
 TEMP_DIR = f'../{ACTIVATION}/temp/'
 IMAGES_DIR = f'../{ACTIVATION}/visualizations/'
+INITIALIZER = lambda : RandomNormal(0, 1e-2)
+KERNEL = 3
 
 ## Set up directories
 for i in [DATA_DIR, TEMP_DIR, IMAGES_DIR, MODELS_DIR]:
     if not os.path.exists(i):
         os.makedirs(i)
-for i in range(1,5):
+for i in range(1,7):
     if not os.path.exists(f'{IMAGES_DIR}conv{i}/'):
         os.makedirs(f'{IMAGES_DIR}conv{i}/')
 
@@ -116,13 +120,15 @@ def make_classifier():
     Return the classifier and a sub-model with output at each major layer
     '''
     input_layer = layers.Input((28, 28, 1))
-    conv1 = layers.Conv2D(filters=6, kernel_size=3, padding='same', activation=ACTIVATION)(input_layer)
-    conv2 = layers.Conv2D(filters=6, kernel_size=3, padding='same', activation=ACTIVATION)(conv1)
-    mp1 = layers.MaxPooling2D(pool_size=2, padding='same')(conv2) # 14x14x16
-    conv3 = layers.Conv2D(filters=12, kernel_size=3, padding='same', activation=ACTIVATION)(mp1)
-    conv4 = layers.Conv2D(filters=12, kernel_size=3, padding='same', activation=ACTIVATION)(conv3)
-    mp2 = layers.MaxPooling2D(pool_size=2, padding='same')(conv4) # 7x7x32
-    flattened = layers.Flatten()(mp2)
+    conv1 = layers.Conv2D(filters=4, kernel_size=KERNEL, padding='same', activation=ACTIVATION, kernel_initializer=INITIALIZER())(input_layer)
+    conv2 = layers.Conv2D(filters=4, kernel_size=KERNEL, padding='same', activation=ACTIVATION, kernel_initializer=INITIALIZER())(conv1)
+    mp1 = layers.MaxPooling2D(pool_size=2, padding='same')(conv1) # 14x14
+    conv3 = layers.Conv2D(filters=8, kernel_size=KERNEL, padding='same', activation=ACTIVATION, kernel_initializer=INITIALIZER())(mp1)
+    conv4 = layers.Conv2D(filters=8, kernel_size=KERNEL, padding='same', activation=ACTIVATION, kernel_initializer=INITIALIZER())(conv3)
+    mp2 = layers.MaxPooling2D(pool_size=2, padding='same')(conv3) # 7x7
+    conv5 = layers.Conv2D(filters=16, kernel_size=KERNEL, padding='same', activation=ACTIVATION, kernel_initializer=INITIALIZER())(mp2)
+    conv6 = layers.Conv2D(filters=16, kernel_size=KERNEL, padding='same', activation=ACTIVATION, kernel_initializer=INITIALIZER())(conv5)
+    flattened = layers.Flatten()(conv5)
     dense1 = layers.Dense(64, activation='relu')(flattened)
     dense2 = layers.Dense(32, activation='relu')(dense1)
     output_layer = layers.Dense(10, activation='softmax')(dense2)
@@ -131,14 +137,16 @@ def make_classifier():
     model_conv2 = Model(input_layer, conv2)
     model_conv3 = Model(input_layer, conv3)
     model_conv4 = Model(input_layer, conv4)
+    model_conv5 = Model(input_layer, conv5)
+    model_conv6 = Model(input_layer, conv6)
     model_dense1 = Model(input_layer, dense1)
     model_dense2 = Model(input_layer, dense2)
     classifier = Model(input_layer, output_layer)
     classifier.compile('adam', loss='categorical_crossentropy', metrics=['accuracy'])
 
-    return model_conv1, model_conv2, model_conv3, model_conv4, model_dense1, model_dense2, classifier
+    return model_conv1, model_conv2, model_conv3, model_conv4, model_conv5, model_conv6, model_dense1, model_dense2, classifier
 
-model_conv1, model_conv2, model_conv3, model_conv4, model_dense1, model_dense2, classifier = make_classifier()
+model_conv1, model_conv2, model_conv3, model_conv4, model_conv5, model_conv6, model_dense1, model_dense2, classifier = make_classifier()
 
 ## Set up lists for storing data
 data_dict = {
@@ -151,6 +159,8 @@ data_dict = {
     'conv2_activations': [], 
     'conv3_activations': [], 
     'conv4_activations': [], 
+    'conv5_activations': [], 
+    'conv6_activations': [], 
     'dense1_activations': [], 
     'dense2_activations': [], 
     'classifier_activations': [], 
@@ -169,6 +179,8 @@ data_dict['conv1_activations'].append(model_conv1.predict(examples))
 data_dict['conv2_activations'].append(model_conv2.predict(examples))
 data_dict['conv3_activations'].append(model_conv3.predict(examples))
 data_dict['conv4_activations'].append(model_conv4.predict(examples))
+data_dict['conv5_activations'].append(model_conv5.predict(examples))
+data_dict['conv6_activations'].append(model_conv6.predict(examples))
 data_dict['dense1_activations'].append(model_dense1.predict(examples))
 data_dict['dense2_activations'].append(model_dense2.predict(examples))
 data_dict['classifier_activations'].append(classifier.predict(examples))
@@ -196,6 +208,8 @@ for epoch in range(EPOCHS):
             data_dict['conv2_activations'].append(model_conv2.predict(examples))
             data_dict['conv3_activations'].append(model_conv3.predict(examples))
             data_dict['conv4_activations'].append(model_conv4.predict(examples))
+            data_dict['conv5_activations'].append(model_conv5.predict(examples))
+            data_dict['conv6_activations'].append(model_conv6.predict(examples))
             data_dict['dense1_activations'].append(model_dense1.predict(examples))
             data_dict['dense2_activations'].append(model_dense2.predict(examples))
             data_dict['classifier_activations'].append(classifier.predict(examples))
@@ -212,6 +226,8 @@ model_conv1.save(f'{MODELS_DIR}model_conv1.h5')
 model_conv2.save(f'{MODELS_DIR}model_conv2.h5')
 model_conv3.save(f'{MODELS_DIR}model_conv3.h5')
 model_conv4.save(f'{MODELS_DIR}model_conv4.h5')
+model_conv5.save(f'{MODELS_DIR}model_conv5.h5')
+model_conv6.save(f'{MODELS_DIR}model_conv6.h5')
 model_dense1.save(f'{MODELS_DIR}model_dense1.h5')
 model_dense2.save(f'{MODELS_DIR}model_dense2.h5')
 classifier.save(f'{MODELS_DIR}classifier.h5')
@@ -229,19 +245,20 @@ plt.ylabel('Metric')
 plt.savefig(f'{IMAGES_DIR}metrics.png')
 plt.close()
 
-for conv in range(1,5):
+for conv in range(1,7):
     print(f'Saving images for conv{conv}')
     biggest = np.max(data_dict[f'conv{conv}_activations'])
     #biggets = 1
     smallest = np.min(data_dict[f'conv{conv}_activations'])
-    smallest = 0
+    #smallest = -1
     for index, i in enumerate(data_dict[f'conv{conv}_activations']):
         plot_images(examples, i, f'{IMAGES_DIR}conv{conv}/act.conv{conv}.{index:05d}.png', vmin=smallest, vmax=biggest)
-    os.system(f'ffmpeg -r 8 -i {IMAGES_DIR}conv{conv}/act.conv{conv}.%05d.png'
+    os.system(f'ffmpeg -r 10 -i {IMAGES_DIR}conv{conv}/act.conv{conv}.%05d.png'
               ''' -vf "drawtext=fontfile=Arial.ttf: text='%{frame_num}':'''
               ''' start_number=1: x=0: y=0: fontcolor=black: fontsize=20:'''
               ''' box=1: boxcolor=white: boxborderw=5"'''
               f' -crf 15 {IMAGES_DIR}conv{conv}.mp4')
+
 '''
 print('Begin JSON dump...')
 with open(f'{DATA_DIR}data.json', 'w') as f:
